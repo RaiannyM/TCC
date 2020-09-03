22c0b0791ec87af36f43d4fb9481f1464d0553cb
Raianny Magalhães <33043937+RaiannyM@users.noreply.github.com>
2020-09-03 13:10:37 -0300

    03/09

---
+++ Assets/Scripts/Fluxo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo : MonoBehaviour {
+	public void CarregarListaDeCuidados(){
+		SceneManager.LoadScene ("lista_de_cuidados");
+	}
+}
+
---
+++ Assets/Scripts/FluxoMenu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FluxoMenu : MonoBehaviour {
+	public void CarregarAmbiente(){
+		SceneManager.LoadScene ("ambiente");
+	}
+}
