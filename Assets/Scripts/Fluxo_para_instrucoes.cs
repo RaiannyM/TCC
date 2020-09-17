3f54c46aae85d806c12afcc993755714c7ef14f9
Raianny Magalhães <33043937+RaiannyM@users.noreply.github.com>
2020-09-16 23:24:15 -0300

    16/09/2020

---
+++ Assets/Scripts/Fluxo_para_instrucoes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo_para_instrucoes : MonoBehaviour {
+	public void CarregarInstrucoes(){
+		SceneManager.LoadScene ("instrucoes");
+	}
+}
