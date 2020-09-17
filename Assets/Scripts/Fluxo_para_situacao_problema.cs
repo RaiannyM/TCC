5a8486546ac8beabcbaad2022450d31940478f85
Raianny Magalhães <33043937+RaiannyM@users.noreply.github.com>
2020-09-17 00:40:32 -0300

    16/09/2020 .2

---
+++ Assets/Scripts/Fluxo_para_situacao_problema.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fluxo_para_situacao_problema : MonoBehaviour {
+	public void CarregarSituacaoProblema(){
+		SceneManager.LoadScene ("situacao_problema");
+	}
+}
